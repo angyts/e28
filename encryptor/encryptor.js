43381e94e30fa05b4c2760513485c8725af57f2d
paul_ang <23327200+punggolzenith@users.noreply.github.com>
2020-02-21 13:10:49 +0800

    created an encryptor app for PGP

---
+++ encryptor/encryptor.js
@@ -0,0 +1,81 @@
+// put keys in backtick (``) to avoid errors caused by spaces or tabs
+const publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+Comment: User-ID:\tDr Paul Ang Teng Soon <drpaulang@punggolgp.com>
+Comment: Created:\t6/10/2019 2:46 AM
+Comment: Expires:\t6/10/2024 12:00 PM
+Comment: Type:\t4096-bit RSA (secret key available)
+Comment: Usage:\tSigning, Encryption, Certifying User-IDs
+Comment: Fingerprint:\t08A009DE9CD7F259F3A782CEE048DF7B43DB7C74
+
+
+mQINBF2Y5Q0BEAC/mA/Gpm+8ZzZXSaD90yJj+z99Kibgr0Y3WIzPIY52qdqgr3iH
+6fkJ2WLjP7Yf4RrQXevsFSg6fhWskcKxYPmjY8hZDdEsz0ro+AHad9GmUOxbmFxD
+umUjpuFgsv9trZjdWaA20al9S5aSBSB8dkkoKg0hM8ru4oWadkJ2O7UA21teNN2f
+eTUB5KknSAwfpSGFHMD+2zNBdQLNrnLa3tdzcQYng8DfHRfaQc59JMq6ubqsIXLQ
+rjKuh5XVfS58OXVD6kr/mHxeULcqCAVPWWfNbnvmDxdC6RtOzkhtaaCOheQO7n/W
+5sDHQ3ioktpS0NfpKa9gjEjrCLZmaeH6OIDAqX43I7Fy0272xnUgf2HhVEtSnjHh
+WN/pKGCkjdamHrhBPrXjw90Y6s4r7PiB3O8OgwJ8wMqfTazO5qmdxBxsJG70GWPl
+c5lIf5SdahrlUHB6MfgfVrEUytvTSrYChN+BsHGdoE/0IT9C+lkcVvPle37m11Rv
+12sUK7yONmEn+07bVLoJ0nOgQvKGSYy/nxRBLp5m1Z3zu9eccok8oDi1GCGOySKW
+glvD1Q2RUrDR4gFjyGo4dyALI3CsI/7nhRf8jd4nzHbTuuM7W4DFaU53Nm266ust
+6a8TTA7Eu9RFxBrXXijUmmStXOiNFlUWDGOR5/X1oWj9z3bUXMthe9f6DQARAQAB
+tC9EciBQYXVsIEFuZyBUZW5nIFNvb24gPGRycGF1bGFuZ0BwdW5nZ29sZ3AuY29t
+PokCVAQTAQgAPhYhBAigCd6c1/JZ86eCzuBI33tD23x0BQJdmOUNAhsDBQkJaSYz
+BQsJCAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEOBI33tD23x0EPoQAIng43DoH1L4
+wgNCG9g2jVK6ro1n1wQvYSO/f9qa/o18l5CuwvYBDeTKWOQUrznpDzdQ0zzgZIjI
+6z3967Ff/fJ6T5fVDUoyQx0NYRL2mXf2qnlPPHGUU0XDycoWsC/776nq1U3fnjpF
+KlGyWTZSDHcWe4Yl/U8DyPI96tS6HI+1dvr69EVoBI7qLA9rH5B8cYaCrNRZqeHc
+BEySBZIFwQ8y16FSWVLbdKqpByAyFDjWo0iTxZK1sNK0QgcwsDp6xnt/UroNFbKq
+e5cXwBi6LDgcWmpt3p47HXdxCNttHYVXilSbkVKC0nGITQ1i+uy+/wYT8oJX91Ia
+y/RhbyPH9oa8gzMJHI0We3dtTV5C0I3cIz4XNxkSX5g/YaA5ybOs7gGa1iua5xtN
+PDnQScj0xBUaGK8B7ppEJEPGS0kv/WAPC357w8W2P54pCYNI8uKhup0oU2Uwq73G
+Pr4KFNM3Ih+6Ck3MKqHwaSN01cLgm2cgzhPOIztfG6NQcllVLovDXByjFqs4VMJ1
+4bSqJ1zDBIjiQ/+Jep+b/F5HIFMf2s3H7o9/uvBoyRp2olq177oq+onmV3Bi9tSs
+A9+5csNMrE41VTZ8gvrGA3ukORVJPlZm0BWMGcvz4S684vjZg+RQBXnTZc573LHW
+Y+jXBzh1YjgakaU2yc/YCC9KEiZ2jYaMuQINBF2Y5Q0BEADoudiXxM26qMnGWdiI
+1GKj9DYRhOwzAvRAfdhAMDLIwRcYqOvZ5cMY/QY7hRzSsoCIFZ8EN0Fiw0+pLOVU
+FPHgFRM5OmECZ8G8WjZVIaW+sQA6sNqT6qkCqIzQo8fqP+Ld/yHsbslKkn5kYKdy
+E9m2YqEsAZKJOAOyph0E1bAqG0/eS55ynTnWzQHR8UbEC84hJiW6VUQHw6jXze5T
+0H1k6cf77oclRRshd75GCdqEPKglvLo9hikd0ygVKjPXMK/d685fYdLyzg/FW4JP
+0vHHPS4b1mSX7KWlsyWjpdSlQE+IOqO4FWjYv5srx75i95LlwVM9hgIfEIwPzQSY
+TkfaC26LJEAxpluBpp5zT7WbrSBXdxwW95NSmXld933sBglxbSbSVDjuvxftTUwT
+F+rJ0X+LXHxPRi5xp0V1Dd7dlHPeX+HbAZxQ/8Dm+iY/TsSRlJDaY7BFWThK5Qa5
+ZyN8JjShHlND8kYZ3wUQ3AMXwTN+T5RmEotRxO14NgUcVTM5rqJ54R3joV2FCcrl
+iUEQ/CHaww7rlUJneIJ0QChvW3mNtgCjValA4vFSSFP/CESkZ1VJiKV8kPG01FAb
+UAjvBip7v3OcsJfylV8efUJBrd01jZvAYqGEs7z6eDqYgcOcWLAXRckyhHBsADBc
+ccdDcgX7FvrttPnsGZn6NQw7gQARAQABiQI8BBgBCAAmFiEECKAJ3pzX8lnzp4LO
+4Ejfe0PbfHQFAl2Y5Q0CGwwFCQlpJjMACgkQ4Ejfe0PbfHTZyg/9HbAghhxi6R6W
+8+QFHowaC98DkomlRyrlrDGvCDEgsPOquIWLegSmQBVTVmieMIjDlLSF6BLNvdjT
+RB/mMayMxqGF/XBfZKMItql5WjKzkskljnd3b0Ga/xOGneLkeN5A9BrMGFeHgaU+
+pjOMXObMv7FzZsXb614Y4LDC4syWMYQ51Rr6qo0tAzW+C0iwEXNTT5O0puaseohg
+PwNeckhKljupSCNHuyxRBMysyJSKmARpAAuB89V4hzMzby92uy+BhN/9qN86sqe6
+D234/86PRUHUlyqsqHosWaa+6c/OB7ux4Y45TBzhyyOaputGv9wI4eTJvy2uhhWM
+mXqNbkT7ah5s5VVQjGXt4NQfsAz5s01stqHEYt/+AUCGMqfveo31p2M8c7iSqHw4
+zd8xLXFh2BV2Rh/nUHF9XS8VRLUkh1Qc7qgB153W/nMcsCO49MDB+tk2QKGyYRTx
+saJc9agd2yoj8YbLm41vxRJKLcy3txBb491eegvpUa8L0Wh1MIuHZIZAUVIKZtaq
+XeVTHzJ1q1cAw3gHhjOPV7JnWfKnZGIcCMQ6UUlgGDW3stw/ntqkqswbCxBMdJrw
+HT6QwuT5Ip9cSNTjZbaHCMwpkfpnNfUyf37aU/sscj1JsdVnWY8irW0v6uF9mt3b
+5XuoZugM8l3LWGBYiqxUYNjyNUabyWI=
+=F5PU
+-----END PGP PUBLIC KEY BLOCK-----`;
+
+async function foo(msg) {
+    const {data: encrypted} = await openpgp.encrypt({
+        message: openpgp.message.fromText(`${msg}`),                 // input as Message object
+        publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys // for encryption
+    });
+    console.log(encrypted); // '-----BEGIN PGP MESSAGE ... END PGP MESSAGE-----'
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: ''
+    },
+    computed: {},
+    methods: {
+        encryptThis: function () {
+            foo(this.message);
+        }
+    }
+})
